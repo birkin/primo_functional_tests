424c6a4478403756a7f020282f8496b4510d0bb7
Birkin James Diana <birkin.diana@gmail.com>
2022-05-19 22:32:13 -0400

    shows pool with lock, with all working.

---
+++ demo_mp_f_with_lock.py
@@ -0,0 +1,63 @@
+"""
+For 'lock' info, see <https://stackoverflow.com/a/61265000>.
+"""
+
+import json, pprint, random, time, timeit
+from multiprocessing import current_process, Pool, Lock
+
+
+JOBS = list( range(1000) )
+NUM_WORKERS = 100
+TRACKER_FILE_PATH = '../output_file/test_output.json'
+
+
+def process_job( job ):
+    process_name = current_process().name
+    print( f'starting process, ``{process_name}`` with job, ``{job}``' )
+    start = timeit.default_timer()
+    delay: int = job
+    time.sleep( delay )
+    end = timeit.default_timer()
+    elapsed: str = str( end - start )
+    print( f'process, ``{current_process().name}`` took, ``{elapsed}``' )
+    update_tracker( job, process_name, elapsed )
+    return
+
+
+def update_tracker( job, process_name, elapsed ):
+    with lock_manager:
+        print( f'lock acquired by process, ``{process_name}``' )
+        msg = f'job, ``{job}`` completed by process, ``{process_name}`` in, ``{elapsed}``'
+        with open( TRACKER_FILE_PATH, 'r' ) as reader:
+            jlist = json.loads( reader.read() )
+            jlist.append( msg )
+            with open( TRACKER_FILE_PATH, 'w' ) as writer:
+                jsn = json.dumps( jlist, indent=2 )
+                writer.write( jsn ) 
+        print( 'tracker updated' )
+        print( f'lock released by process, ``{process_name}``' )
+    return
+
+def initialize_pool( lock ):
+    global lock_manager
+    lock_manager = lock
+
+if __name__ == '__main__':
+    start = timeit.default_timer()
+    ## set up tracker ---------------------------
+    with open( TRACKER_FILE_PATH, 'w' ) as writer:
+        jsn = json.dumps( [] )
+        writer.write( jsn ) 
+    ## set up jobs ------------------------------
+    jobs = []
+    for i in JOBS:
+        delay: float = ( random.randint(400, 600) / 1000 )
+        jobs.append( delay )
+    ## start workers and send jobs --------------
+    lock = Lock()
+    with Pool( NUM_WORKERS, initializer=initialize_pool, initargs=[lock] ) as workers:
+        rslt = workers.map( process_job, jobs )
+    ## wind down --------------------------------
+    end = timeit.default_timer()
+    elapsed: str = str( end - start )
+    print( f'total-elapsed, ``{elapsed}``' )
