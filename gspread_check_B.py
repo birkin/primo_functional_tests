7532c795021739df300bdebdca3836540684e28b
Birkin James Diana <birkin.diana@gmail.com>
2022-05-28 07:59:07 -0400

    loads credentials from envar.

--- gspread_check_B.py
+++ gspread_check_B.py
@@ -5,29 +5,8 @@ import gspread
 CREDENTIALS: dict = json.loads( os.environ['PRIMO_F_TESTS__SHEET_CREDENTIALS_JSON'] )
 SPREADSHEET_NAME = os.environ['PRIMO_F_TESTS__SHEET_NAME']
 
-pprint.pprint( CREDENTIALS )
+
 gc = gspread.service_account_from_dict( CREDENTIALS )
-
-# pprint.pprint( credentials )
-# gc = gspread.service_account_from_dict( credentials )
-
 sh = gc.open( SPREADSHEET_NAME )
 
 print(sh.sheet1.get('A1'))
-
-
-
-
-# import os
-# import gspread
-
-
-# CREDENTIALS_PATH = os.environ['PRIMO_F_TESTS__SHEET_CREDENTIALS_PATH']
-# SPREADSHEET_NAME = os.environ['PRIMO_F_TESTS__SHEET_NAME']
-
-
-# gc = gspread.service_account( filename=CREDENTIALS_PATH )
-
-# sh = gc.open( SPREADSHEET_NAME )
-
-# print(sh.sheet1.get('A1'))
